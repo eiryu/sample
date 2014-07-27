9129ab55ecf001337da857ae6d86d33d079d64f5
eiryu <eiryu.co@gmail.com>
2014-07-28 03:13:22 +0900

    array to collection

--- src/test/java/com/eiryu/sample/GuavaTests.java
+++ src/test/java/com/eiryu/sample/GuavaTests.java
@@ -1,7 +1,10 @@
 package com.eiryu.sample;
 
 import com.google.common.base.Strings;
+import com.google.common.primitives.Ints;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -16,9 +19,20 @@ import static org.junit.Assert.assertThat;
 public class GuavaTests {
 
     @Test
-    public void a_null空チェック(){
+    public void a_null空チェック() {
         assertThat(Strings.isNullOrEmpty(null), is(true));
         assertThat(Strings.isNullOrEmpty(""), is(true));
         assertThat(Strings.isNullOrEmpty("a"), is(false));
     }
+
+    @Test
+    public void b_プリミティブ配列からコレクションに変換() {
+        int[] intArray = {1, 2, 3};
+        List<Integer> integerList = Ints.asList(intArray);
+
+        assertThat(integerList.get(0), is(1));
+        assertThat(integerList.get(1), is(2));
+        assertThat(integerList.get(2), is(3));
+    }
+
 }
